6deb0dfde45f353b917d3a7c7d1936348d9f98d0
priscila0621 <pjulieths@uamv.edu.ni>
2026-05-13 17:21:20 -0600

    chore: initialize Spring Boot project

---
+++ src/main/java/ni/edu/uam/bibliotecaapi/BibliotecaapiApplication.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.bibliotecaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ni/edu/uam/bibliotecaapi/BibliotecaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.edu.uam.bibliotecaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
